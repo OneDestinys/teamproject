d351284fce70d648e3380c2eef6ff9aeddf94e7c
Jacob <122861394+OneDestinys@users.noreply.github.com>
2023-04-27 14:51:59 -0400

    ticket system

--- discord_bot.py
+++ discord_bot.py
@@ -7,21 +7,22 @@ from discord.ext import commands
 from discord.ext.bridge import Bot, BridgeContext, has_permissions
 from discord.ui import Button, View
 import discord
-from settings import TOKEN
+from settings import TOKEN, MONGODBURL
+from pymongo import MongoClient
 
 # Creating bot object and assigning it to a variable, passing my command prefix and permissions as parameters
 my_bot = Bot(command_prefix="!", intents=discord.Intents.all())
+client = MongoClient(MONGODBURL)
+database = client["Cluster0"]
+tickets = database["Cluster0"]
 
 
-# class My_Buttons(Button):
-#      def __init__(self, label):
-#           super().__init__(label =label, style=None)
-#      async def button_callback(self, interaction):
-#           await interaction.response.send_message("Button 1!")
-# Will print bot's username
+# https://pymongo.readthedocs.io/en/stable/tutorial.html
 @my_bot.event
 async def on_ready():
     print(f"Now running {my_bot.user.name}...")
+    my_bot.add_view(Panel_Options())
+    # adding my class into the possible selectable items you can view
 
 
 # bridge command will await message with prefix or /
@@ -46,22 +47,62 @@ async def ban_error(ctx: discord.ApplicationContext, error: discord.DiscordExcep
     print(error)
 
 
+class Panel_Options(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        # calling super to override the view class, setting timeout = none so the bot recognizes my buttons even after a restart
+
+    @discord.ui.button(
+        label="Open a Ticket", custom_id="1", style=discord.ButtonStyle.primary
+    )
+    async def callback(self, btn, interaction: discord.Interaction):
+        await interaction.response.defer()
+        # the bot only has 5 seconds to respond and this can be slow, so we need to let it 'think' for a bit
+        Create_Category = True
+        category = None
+        for item in interaction.guild.categories:
+            if "tickets" == item.name:
+                Create_Category = False
+                category = item
+        if Create_Category:
+            category = await interaction.guild.create_category(name="tickets")
+        # waiting for interaction to finish so it can make the ticket
+
+        for channel in category.text_channels:
+            if channel.topic == interaction.user.id:
+                await interaction.response.send_message("You already have a ticket")
+                return
+
+        ticket_id = tickets.estimated_document_count() + 1
+        ticket = await interaction.guild.create_text_channel(
+            name=f"ticket-{ticket_id}",
+            category=category,
+            overwrites={
+                interaction.user: discord.PermissionOverwrite(view_channel=True, send_messages=True, read_messages=True),
+                interaction.guild.default_role: discord.PermissionOverwrite(view_channel=False)
+            },
+            topic=f'{interaction.user.id}'
+        )
+        tickets.insert_one({
+            "creator": interaction.user.id,
+            "channel": ticket.id
+        })
+
+
 @my_bot.command()
-async def hello(ctx):
-    button = Button(label="Click me!", style=discord.ButtonStyle.primary)
-    button2 = Button(label="Click me!")
-
-    button.callback = button_callback
-    view = View()
-
-    async def button_callback(self, interaction):
-        await interaction.response.send_message("Button 1!")
-
-    view.add_item(button)
-    view.add_item(button2)
-    await ctx.send("Hi!", view=view)
+async def panel(ctx):
+    await ctx.send(
+        view=Panel_Options(),
+        embeds=[
+            discord.Embed(
+                color=0xA020F0,
+                title="Ticket Panel",
+                description="Click on the button below to make a ticket",
+            )
+        ],
+    )
     # https://www.youtube.com/watch?v=kNUuYEWGOxA
-    # ChatGPT to figure out I needed view=view
+    # https://towardsdatascience.com/binary-hex-and-octal-in-python-20222488cee1
 
 
 my_bot.run(TOKEN)
