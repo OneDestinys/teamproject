f5b12f73dba2850a19d7ee80d1520cbd30d55058
Jacob <122861394+OneDestinys@users.noreply.github.com>
2023-04-28 19:36:04 -0400

    ignore

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-TOKEN = ""
-MONGODBURL = ""
+TOKEN = "MTEwMDEwNjU3MzUzMzE2MzYzMg.GvwPej.xYMUYB-t-j0P0ScOkIQ7gu7otM0Rb0CWCHZoMk"
+MONGODBURL = "mongodb+srv://jacobfoti:52YQttp8CjHEjSc0@cluster0.unr2yyp.mongodb.net/?retryWrites=true&w=majority"
